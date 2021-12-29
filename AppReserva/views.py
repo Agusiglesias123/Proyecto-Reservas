f552af39fccb6646223ad61481b209933dd1c668
Iglesias Agustin <agustiniglesias09@hotmail.com>
2021-12-29 03:31:43 -0300

    v.0.0.1

---
+++ AppReserva/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+from .forms import Solicitud
+from .models import Reserva
+
+# Create your views here.
+
+def index(request):
+
+    return render(request, "index.html")
+
+def login(request):
+
+    return render(request, "login.html")
+
+
+def gestion(request):
+    reserva= Reserva.objects.all()
+    return render(request,'gestion.html',{'reserva':reserva})
+
+def galeria(request):
+    if request.method == 'POST':
+        form= Solicitud(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = Solicitud()
+    return render(request, 'galeria.html', {'form':form})

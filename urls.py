cde38d81264a603b9cf57fcde3039b531a720c5c
Aibn666 <85924694+Aibn666@users.noreply.github.com>
2022-01-02 20:36:04 -0300

    v.0.0.5

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+
+from .import views
+
+
+urlpatterns = [
+    path('', views.index, name="index.html"),
+    path('galeria/', views.galeria, name="galeria.html"),
+    path('gestion/', views.gestion, name="gestion.html"),
+    path('login/', views.login, name="login.html"),
+    path('eliminar/<int:id_reserva>', views.eliminar, name="eliminar"),
+
+
+]

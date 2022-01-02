89364f170a56577f3055a6c2fca285a7f1de971f
Aibn666 <85924694+Aibn666@users.noreply.github.com>
2022-01-02 20:37:56 -0300

    v.0.0.5

--- AppReserva/urls.py
+++ AppReserva/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('galeria/', views.galeria, name="galeria.html"),
     path('gestion/', views.gestion, name="gestion.html"),
     path('login/', views.login, name="login.html"),
+    path('eliminar/<int:id_reserva>', views.eliminar, name="eliminar"),
 
 
 ]
--- AppReserva/views.py
+++ AppReserva/views.py
@@ -23,6 +23,13 @@ def galeria(request):
         form= Solicitud(request.POST)
         if form.is_valid():
             form.save()
+            form = Solicitud()
     else:
         form = Solicitud()
     return render(request, 'galeria.html', {'form':form})
+
+def eliminar(request, id_reserva):
+    reserva = Reserva.objects.get(pk=id_reserva)
+    reserva.delete()
+    reserva = Reserva.objects.all()
+    return render(request,'gestion.html',{'reserva':reserva})
